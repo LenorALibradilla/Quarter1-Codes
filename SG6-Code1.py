2a17be478a63848bfc3ed992ccfc6b91bbf7c10c
LenorALibradilla <lenoranne.libradilla@smc.pshs.edu.ph>
2025-09-09 10:40:42 +0800

    Study Guide 6-Code 1

--- SG6-Code1.py
+++ SG6-Code1.py
@@ -3,3 +3,5 @@ num2 = float(input("Enter second number: "))
 num3 = float(input("Enter third number: "))
 sum = num1 + num2 + num3
 rounded_sum = round(sum, 2)
+formatted_sum = "{:.2f}".format(rounded_sum)
+print("The total of the three numbers is:", formatted_sum)
