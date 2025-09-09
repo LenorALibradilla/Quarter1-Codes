e3b85300b347490890e66fa46e36e5fbb292ad9e
LenorALibradilla <lenoranne.libradilla@smc.pshs.edu.ph>
2025-09-09 10:32:58 +0800

    Study Guide 6-Code 1

---
+++ SG6-Code1.py
@@ -0,0 +1,5 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+sum = num1 + num2 + num3
+rounded_sum = round(sum, 2)
